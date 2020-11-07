06a634de7c20cce1f6404cae05f046ecceafd7bd
Kenneth-Umphenour-gamedev <64235264+Kenneth-Umphenour-gamedev@users.noreply.github.com>
2020-11-07 15:35:26 -0600

    Add files via upload

---
+++ Code/Control Bindings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Control Bindings", menuName = "Keybindings")]
+public class ControlBindings : ScriptableObject
+{
+   public KeyCode up, down, left, right, interact, pause;
+}
