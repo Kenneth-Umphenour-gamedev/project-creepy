ee6ba2b4606029c3f4bab9d594d8d133b88117ce
Kenneth-Umphenour-gamedev <64235264+Kenneth-Umphenour-gamedev@users.noreply.github.com>
2020-11-07 15:41:16 -0600

    Delete Control Bindings.cs

--- Code/Control Bindings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Control Bindings", menuName = "Keybindings")]
-public class ControlBindings : ScriptableObject
-{
-   public KeyCode up, down, left, right, interact, pause;
-}
