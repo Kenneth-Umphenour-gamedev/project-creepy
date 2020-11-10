e01fdebd12523ab69ac8cf61da3a84a1aace876e
Kenneth-Umphenour-gamedev <64235264+Kenneth-Umphenour-gamedev@users.noreply.github.com>
2020-11-09 19:25:21 -0600

    Update Control Bindings.cs

--- Assets/Code/Control Bindings.cs
+++ Assets/Code/Control Bindings.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+//-------------------------------------------------------------------------------------
+// <copyright file="Project-creepy/Assets/Code/Control Bindings.cs" compyany="Project-Creepy Dev">
+//    Author: Kenneth Umphenour
+//    Copyright (c) Project-Creepy Dev. All rights reserved.
+// </copyright>
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
